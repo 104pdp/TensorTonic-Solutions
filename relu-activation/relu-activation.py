26bf5d678739d6cf3ca9791acf7286affb1804c1
104pdp <40543670+104pdp@users.noreply.github.com>
2026-01-14 15:18:35 +0300

    Update solution for relu-activation

--- relu-activation/relu-activation.py
+++ relu-activation/relu-activation.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def relu(x):
-    """
-    Implement ReLU activation function.
-    """
-    x = np.asarray(x, dtype=float)
-    y = np.maximum(0.0, x)
-    return np.atleast_1d(y)
+
+    x_arr = np.asarray(x, dtype=float)
+    y = np.maximum(0.0, x_arr)
+
+    # Если скаляр — вернуть массив формы (1,)
+    if y.ndim == 0:
+        return np.array([y], dtype=float)
+
+    return y
