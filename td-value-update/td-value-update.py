f7a620531cbab9eff461f920eaad8d15fece0991
104pdp <40543670+104pdp@users.noreply.github.com>
2026-01-14 15:55:41 +0300

    Add solution for td-value-update

---
+++ td-value-update/td-value-update.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def td_value_update(V, s, r, s_next, alpha, gamma):
+    """
+    Returns: updated value function V_new
+    """
+    V = np.asarray(V, dtype=float)
+    V_new = V.copy()
+
+    td_error = float(r) + float(gamma) * V[s_next] - V[s]
+    V_new[s] = V[s] + float(alpha) * td_error
+
+    return V_new
